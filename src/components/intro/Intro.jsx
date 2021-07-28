e87029bc3abf067ad2e9048cb2fcb943afadea72
Mohammed Ashour <alex.ashour92@gmail.com>
2021-07-28 11:21:16 -0400

    introduction page

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -2,8 +2,22 @@ import "./intro.scss"
 
 export default function Intro() {
     return (
-        <div className="intro">
-            
+        <div className="intro" id="intro">
+            <div className="left">
+                <div className="imgContainer">
+                    <img src="assets/man.png" alt=""/>
+                </div>
+            </div>
+            <div className="right">
+                <div className="wrapper">
+                    <h2>Hi There, I'm</h2>
+                    <h1>Mohammed Ashour</h1>
+                    <h3>Freelance <span>Developer</span></h3>
+                </div>
+                <a href="#portfolio">
+                    <img src="assets/down.png" alt="" />
+                </a>
+            </div>
         </div>
     )
 }
