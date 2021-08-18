1acc78feb67989bb3b0d9ef84fdf9532b9708ac9
Mohammed Ashour <alex.ashour92@gmail.com>
2021-08-18 11:26:05 -0400

    last

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,6 +1,20 @@
-import "./intro.scss"
+import "./intro.scss";
+import { init } from 'ityped';
+import { useEffect, useRef } from "react";
 
 export default function Intro() {
+const textRef = useRef();
+
+    useEffect(() => {
+        init(textRef.current,{
+             showCursor: true,
+             backDelay:  1500,
+             backSpeed:60,
+            
+              strings: ["Developer", "Designer", "Content Creator"] 
+            })
+    
+    }, [])
     return (
         <div className="intro" id="intro">
             <div className="left">
@@ -12,7 +26,7 @@ export default function Intro() {
                 <div className="wrapper">
                     <h2>Hi There, I'm</h2>
                     <h1>Mohammed Ashour</h1>
-                    <h3>Freelance <span>Developer</span></h3>
+                    <h3>Freelance <span ref={textRef}></span></h3>
                 </div>
                 <a href="#portfolio">
                     <img src="assets/down.png" alt="" />
